827e7300fbd24603d31427e381c1acd87f6627ba
Yuriy Padlyak <Yuriy.Padlyak@solarwinds.com>
2020-11-20 13:18:19 -0600

    Implement Meraki polling cache and rate limiting [NPM-16406]

---
+++ Src/SolarWinds.SharedCommunication/DataCache/SingleProcessDataCacheFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SolarWinds.SharedCommunication.Contracts.DataCache;
+using SolarWinds.SharedCommunication.Contracts.Utils;
+
+namespace SolarWinds.SharedCommunication.DataCache
+{
+    public class SingleProcessDataCacheFactory : IDataCacheFactory
+    {
+        private readonly IDateTime _dateTime;
+
+        public SingleProcessDataCacheFactory(IDateTime dateTime)
+        {
+            _dateTime = dateTime ?? throw new ArgumentNullException(nameof(dateTime));
+        }
+
+        public IDataCache<T> CreateCache<T>(string cacheName, TimeSpan ttl)
+        {
+            return SingleProcessDataCache<T>.Create(cacheName, ttl, _dateTime);
+        }
+    }
+}
