b1e321067b0d6092d8a002a679d11befab204d09
Roman Brych <roman.brych@solarwinds.com>
2020-12-18 10:23:48 -0600

    Add unit tests to PlatformDateTime and SynchronizationIdentifiersProviderTest

---
+++ Src/SolarWinds.SharedCommunication.Tests/Utils/PlatformDateTimeTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using SolarWinds.SharedCommunication.Contracts.Utils;
+using SolarWinds.SharedCommunication.Utils;
+using System;
+
+namespace SolarWinds.SharedCommunication.Tests.Utils
+{
+    public class PlatformDateTimeTests
+    {
+        private IDateTime _platformDateTime;
+
+        [SetUp]
+        public void PlatformDateTimeTests_SetUp()
+        {
+            _platformDateTime = new PlatformDateTime();
+        }
+
+        [Test]
+        public void UtcNowCorrect()
+        {
+            Assert.NotNull(_platformDateTime);
+            Assert.AreEqual(_platformDateTime.UtcNow, DateTime.UtcNow);
+        }
+    }
+}
---
+++ Src/SolarWinds.SharedCommunication.Tests/Utils/SynchronizationIdentifiersProviderTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SolarWinds.SharedCommunication.Contracts.Utils;
+using SolarWinds.SharedCommunication.Utils;
+
+namespace SolarWinds.SharedCommunication.Tests.Utils
+{
+    public class SynchronizationIdentifiersProviderTest
+    {
+        private ISynchronizationIdentifiersProvider _synchronizationIdentifiersProvider;
+
+        [SetUp]
+        public void PlatformDateTimeTests_SetUp()
+        {
+            _synchronizationIdentifiersProvider = new SynchronizationIdentifiersProvider();
+        }
+
+        [TestCase("", "apiKey", "orgId")]
+        [TestCase("BaseAddress", "", "orgId")]
+        public void GetSynchronizationIdentifier_Fails(string apiBaseAddress, string apiKey, string orgId)
+        {
+            Assert.NotNull(_synchronizationIdentifiersProvider);
+            Assert.Throws<ArgumentException>(
+                () => _synchronizationIdentifiersProvider.GetSynchronizationIdentifier(apiBaseAddress, apiKey, orgId));
+        }
+
+        [Test]
+        public void GetSynchronizationIdentifier_Succeeds()
+        {
+            //Arrange
+            var apiBaseAddress = "net.tcp://localhost:17777/SolarWinds/PollerCache";
+            var apiKey = "123";
+            var orgId = "5";
+            var expectedResult = "OH6GkFGoxY2Q+yyXlr2CdYxd3vEnFsmrJP87gO5wehs=";
+            //Act
+            var result =
+                _synchronizationIdentifiersProvider.GetSynchronizationIdentifier(apiBaseAddress, apiKey, orgId);
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsNotEmpty(result);
+            Assert.AreEqual(expectedResult,result);
+        }
+    }
+}
